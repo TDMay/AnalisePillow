a21357909e18a81efad21ee8e071f12f1b141e8d
Maykon <tdmaykongm@gmail.com>
2026-04-25 15:02:27 -0300

    Primeiro codigo, transforma uma imagem jpeg em png

---
+++ app.py
@@ -0,0 +1,7 @@
+from PIL import Image
+
+# Abrindo uma imagem
+imagem = Image.open('/home/may/Code/Pillow/images/image.jpeg')
+
+# Salvando a imagem em outro formato
+imagem.save('/home/may/Code/Pillow/images/nova_imagem.png')
